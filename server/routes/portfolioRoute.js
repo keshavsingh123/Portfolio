c9c72bdc4185f9d8e536a768ebd50c8927196b47
keshav singh <1816514912@kit.ac.in>
2025-01-21 20:05:39 +0530

    rename controller to api

--- server/routes/portfolioRoute.js
+++ server/routes/portfolioRoute.js
@@ -1,8 +1,8 @@
-import express from 'express'
-import { sendEmailController } from '../controllers/portfolioRoute.controller.js'
+import express from "express";
+import { sendEmailController } from "../api/portfolioRoute.controller.js";
 
-export const router = express.Router()
+export const router = express.Router();
 
-router.post('/send-email',(req,res)=>{
-    sendEmailController(req,res)
-})
+router.post("/send-email", (req, res) => {
+  sendEmailController(req, res);
+});
